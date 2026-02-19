d2e90b86b2ca78f4a2cb1f7467e55d93431d71fd
Palash Maji <palash.m@netconnectglobal.com>
2026-02-19 12:47:03 +0530

    initital commit

---
+++ 1.js
@@ -0,0 +1,16 @@
+// jest script
+
+test('test', () => {
+    expect(1).toBe(1);
+});
+
+test('test', () => {
+    expect(1).toBe(1);
+});
+function add(a, b) {
+    return a + b;
+}
+
+test('test', () => {
+    expect(add(1, 2)).toBe(3);
+});
